36e98b0f817dd99ae9e8ef3dd350056a5b34a3a2
anweshan <anweshanaskar@gmail.com>
2015-03-29 11:05:37 -0400

    bubble works

--- popup.js
+++ popup.js
@@ -1,42 +1,29 @@
-window.addEventListener("dblclick", function(event) {
+// Add bubble to the top of the page.
+var bubbleDOM = document.createElement('div');
+bubbleDOM.setAttribute('class', 'selection_bubble');
+document.body.appendChild(bubbleDOM);
+
+// Lets listen to mouseup DOM events.
+document.addEventListener('dblclick', function (e) {
+  var selection = window.getSelection().toString().trim();
+  if (selection.length > 0) {
+
+    renderBubble(e.pageX, e.pageY, selection);
+  }
+}, false);
 
 
-    alertWithText()});
+// Close the bubble when we click on the screen.
+document.addEventListener('mousedown', function (e) {
+  bubbleDOM.style.visibility = 'hidden';
+}, false);
 
-
-
-function getSelectedText() {
-    var text = "";
-    if (typeof window.getSelection != "undefined") {
-        text = window.getSelection().toString();
-    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
-      text = document.selection.createRange().text;
-    }
-    return text;
+// Move that bubble to the appropriate location.
+function renderBubble(mouseX, mouseY, selection) {
+  bubbleDOM.innerHTML = selection + "ADD ALL YOUR STUFF HERE GUISSSSSSSSSSSSSSSSSSSS LESS THAN TWO HOURS LEFT WE GON DIE";
+  bubbleDOM.style.top = mouseY + 'px';
+  bubbleDOM.style.left = mouseX + 'px';
+  bubbleDOM.style.visibility = 'visible';
 }
 
-function alertWithText() {
-    var selectedText = getSelectedText().trim();
-    if (selectedText) {
-        if (selectedText!=""){
-            initHover(selectedText);
-            //alert("You got selected text |" + selectedText+ "|");
-            //window.speechSynthesis.speak(
-        //new SpeechSynthesisUtterance(selectedText)
-//);
-        }
-    }
-}
-
-function initHover(displaytext) {
-$('body')
-       .append('<div id="myLink"></div>');
-    $('#myLink')
-        .append(displaytext);
-}
-
- $(document).ready(function(){
-        $('#myLink').Slide_Label($("<div id='previewA' style='width: 150px; text-align: center'><img src='mingchoi.na.png' width='128' height='128' /><div id='myWords'> This is a Slide Label which made of div element, you can add any HTML5 element like text, photo, music and video inside.</div></div>"));
-        $('#myLink2').Slide_Label($('<div>', {id: 'previewB', style: 'width: 150px'}).append($('<h4>(ノﾟ∀ﾟ)ノ⌒☆</h4>')));
-      });
 
