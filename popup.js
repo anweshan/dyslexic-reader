6acfcc589d811aca789cf38e638d88fba5bc356e
anweshan <anweshanaskar@gmail.com>
2015-03-29 00:54:29 -0400

    can do things with selected text
    it doings it

--- popup.js
+++ popup.js
@@ -1,4 +1,23 @@
 window.addEventListener("dblclick", function(event) { 
 
 
-	alert("hi")});
+	alertWithText()});
+
+
+
+function getSelectedText() {
+    var text = "";
+    if (typeof window.getSelection != "undefined") {
+        text = window.getSelection().toString();
+    } else if (typeof document.selection != "undefined" && document.selection.type == "Text") {
+      text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function alertWithText() {
+    var selectedText = getSelectedText();
+    if (selectedText) {
+        alert("Got selected text " + selectedText);
+    }
+}
